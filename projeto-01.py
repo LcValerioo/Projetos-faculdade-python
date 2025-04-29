978478d3a2d22e5d283ed42bbe5a5a50f1483b3c
Lucas Valério <126265243+LcValerioo@users.noreply.github.com>
2025-04-29 01:54:55 +0000

    compilando...

--- projeto-01.py
+++ projeto-01.py
@@ -1,14 +1,14 @@
 import os 
 
-arquivo1 = open("dados1.txt", 'w', unicode="utf-8")
-printf(os.path.abspath(arquivo1.name))
+arquivo1 = open("dados1.txt", 'w', encoding="utf-8")
+print(os.path.abspath(arquivo1.name))
 # arquivo2 = open("C:\Users\valer\OneDrive\Área de Trabalho\dados2.txt")
 # "manipulação de daods/exemplos/dados1.txt")
 arquivo1.write("Olá mundo !!!")
 
 
-printf(os.path.relpath(arquivo1.name))
-printf(arquivo1)
+print(os.path.relpath(arquivo1.name))
+print(arquivo1)
 
 arquivo1.close()
 
