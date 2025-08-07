c835aa423c8b399cdc4895ad3dc11ee78c054877
Lucas Valério <126265243+LcValerioo@users.noreply.github.com>
2025-08-07 04:20:40 +0000

    implementando CRUD

--- mao_na_massa.py
+++ mao_na_massa.py
@@ -335,3 +335,151 @@ if __name__ == '__main__':
 
     conexao.close()
 """
+"""
+import psycopg2
+from psycopg2 import Error
+
+def connect_to_db():
+    try:
+        #Conectar ao banco de dados
+        connection = psycopg2.connect(
+            host='localhost',
+            database='postgresDB',
+            user='admin',
+            passowrd='admin123'
+        )
+        return connection
+    except Error as e:
+        print(f"Erro ao conectar no Banco de Dados PostgreSQL: {e}")
+        return None
+    
+def create_table():
+    conn = connect_to_db()
+    if conn is not None:
+        cursor = conn.cursor()
+        try:
+            
+            cursor.execute('''
+                            CREATE TABLE IF NOT EXISTS public."AGENDA"
+            (
+                id integer NOT NULL,
+                nome text COLLATE pg_catalog."default" NOT NULL,
+                telefone char(12) COLLATE pg_catalog."default" NOT NULL
+            )
+            TABLESPACE pg_default;
+            ALTER TABLE public."AGENDA"
+                OWNER to admin;
+                    ''')
+            conn.commit()
+        except Error as e:
+            print(f"Erro ao criar Tabela: {e}")
+        finally:
+            cursor.close()
+            conn.close()
+
+    
+def create_contact(nome, telefone):
+    conn = connect_to_db()
+    if conn is not None:
+        cursor = conn.cursor()
+        try:
+            cursor.execute('''
+            INSERT INTO public."AGENDA" (nome, telefone)
+            VALUES (%s,%s) RETURNING id;
+            ''', (nome, telefone))
+            contact_id = cursor.fetchone()[0]
+            conn.commit()
+            print(f"Contato adicionado com ID: {contact_id}")
+        except Error as e:
+            print(f"Erro ao adicionar contato: {e}")
+        finally:
+            cursor.close()
+            conn.close()
+
+def read_contacts():
+    conn = connect_to_db()
+    if conn is not None:
+        cursor = conn.cursor()
+        try:
+            cursor.execute('''
+            SELECT id, nome, telefone FROM public."AGENDA";
+            ''')
+            contacts = cursor.fetchall()
+            for contact in contacts:
+                print(f"ID: {contact[0]}, Nome: {contact[1]}, Telefone: {contact[2]}")
+        except Error as e:
+            print(f"Erro ao ler contatos: {e}")
+        finally:
+            cursor.close()
+            conn.close()
+
+def update_contact(contact_id, novo_nome, novo_telefone):
+    conn = connect_to_db()
+    if conn is not None:
+        cursor = conn.cursor()
+        try:
+            cursor.execute('''
+            UPDATE public."AGENDA"
+            SET nome = %s, telefone = %s
+            WHERE id = %s;
+            ''', (novo_nome, novo_telefone, contact_id))
+            conn.commit()
+            print(f"Contado atualizado com Sucesso!")
+        except Error as e:
+            print(f"Erro ao atualizar contato: {e}")
+        finally:
+            cursor.close()
+            conn.close()
+
+def delete_contact(contact_id):
+    conn = connect_to_db()
+    if conn is not None:
+        cursor = conn.cursor()
+        try:
+            cursor.execute('''
+            DELETE FROM public."AGENDA"
+            WHERE id = %s;
+            ''', (contact_id))
+            conn.commit()
+            print(f"Contato deletado com Sucesso!")
+        except Error as e:
+            print(f"Erro ao excluir contato: {e}")
+        finally:
+            cursor.close()
+            conn.close()
+
+def main():
+    create_table()
+
+    while True:
+        print("\nMenu:")
+        print("1. Adicionar novo contato")
+        print("2. Mostrar todos os contatos")
+        print("3. Atualizar um contato")
+        print("4. Deletar um contato")
+        print("5. Sair")
+        choice = input("Escolha uma opção: ")
+
+        #A partir do python 3.10 é possivel utilizar o match/case
+        if choice == '1':
+            nome = input("Digite um Nome: ")
+            telefone = input("Digite um telefone: ")
+            create_contact(nome, telefone)
+        elif choice == '2':
+            read_contacts()
+        elif choice == '3':
+            contact_id = int(input("Digite o ID do contato para atualizar: "))
+            novo_nome = input("Novo Nome: ")
+            novo_telefone = input("Novo Telefone: ")
+            update_contact(contact_id, novo_nome, novo_telefone)
+        elif choice == '4':
+            contact_id = int(input("Digite o ID do contato para Deletar: "))
+        elif choice == '5':
+            print("Saindo...")
+            break
+        else:
+            print("Opção invalida. Por Favor, tente novamente.")
+
+if __name__ == '__main__':
+    main()
+"""
